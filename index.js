27e3a79dcc8b7f738e2383e61462c43e5bf48c22
Lilmuff1 <prostolexa28@gmail.com>
2022-11-26 13:15:11 +0300

    1 upload

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_1 = require("./lib/config");
+const console_1 = require("./lib/console");
+config_1.config.initialize();
+const menu_1 = require("./lib/menu");
+while (true) {
+    const item = menu_1.menu.choice();
+    if (item!=undefined) {
+        item.run();
+    }
+    (0, console_1.clearConsole)();
+}
